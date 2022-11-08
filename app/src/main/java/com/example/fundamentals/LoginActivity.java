7e5a0aa39e112323e8701a2a3bdea6c2134eea66
Aida2310 <aida_4oct@hotmail.com>
2022-11-08 12:47:57 +0100

    Initial commit

---
+++ app/src/main/java/com/example/fundamentals/LoginActivity.java
@@ -0,0 +1,37 @@
+package com.example.fundamentals;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+   TextView vinculo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        vinculo=findViewById(R.id.textosu);
+        Intent intent= getIntent();
+        vinculo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMain();
+            }
+        });
+    }
+    public void openMain(){
+
+    Intent intent= new Intent(this, SignActivity.class);
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+        startActivity(intent);
+
+    }
+}
